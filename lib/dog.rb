ebf6b0d041b5415cff001206ca112cab0cd4590c
Blake Stoner <stonerblake@gmail.com>
2019-04-23 19:21:20 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+  def bark
+    put "Woof!"
+
+    def sit
+      "the Dog is sitting"
+
+  class Person
+
+  def talk
+    put "Hello World!"
+
+    def walk
+      put "the Person is walking"
+
+      
